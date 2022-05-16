b19c3d4f7536b892bc2a30275f13824a38cc7876
Filipp Babkin <babkin-filipp@mail.ru>
2022-05-17 01:58:58 +0300

    ViewSets applied, few amendments under code

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,24 +1,19 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
-from .views import (
-    CategoryViewSet, GenreViewSet, TitleViewSet,
-    ReviewViewSet, CommentViewSet, register,
-    get_jwt_token, UserViewSet
-)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet, get_jwt_token,
+                    register)
 
-router = routers.DefaultRouter()
-
-router.register('users', UserViewSet, basename='users')
-router.register('categories', CategoryViewSet, basename='categories')
-router.register('genres', GenreViewSet, basename='genres')
-router.register('titles', TitleViewSet, basename='titles')
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet, basename='review')
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet, basename='comments')
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
+router.register(r'titles/(?P<title_id>\d+)/reviews',
+                ReviewViewSet, basename='reviews')
+router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                r'/comments', CommentViewSet, basename='comments')
+router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
